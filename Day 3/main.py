dc02cbdc4371a15bf7ffb9efa7e07c8787b61ffd
lbarto12 <74741908+lbarto12@users.noreply.github.com>
2024-12-03 01:46:33 -0500

    Day 3 Solution - Absurdity

--- Day 3/main.py
+++ Day 3/main.py
@@ -5,5 +5,5 @@ with open('input.txt') as file:
 
     print('Part 1:', sum([a * b for a, b in [(int(c), int(d)) for c, d in re.findall(r'mul\((\d+),(\d+)\)', lines)]]))
 
-    on = True
-    print('Part 2:', sum([int(a) * int(b) for inst, a, b in re.findall(r"(don't|do|mul\((\d+),(\d+)\))", lines) if ((on := inst == 'do') and False if not inst.startswith('mul') else on)]))
+    # I AM HIM
+    print('Part 2:', exec("on = True"), '\b' * 6,  sum([int(a) * int(b) for inst, a, b in re.findall(r"(don't|do|mul\((\d+),(\d+)\))", lines) if ((on := inst == 'do') and False if not inst.startswith('mul') else on)]))
