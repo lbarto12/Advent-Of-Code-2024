14b39a8dfa71c0f01447f03c92eaa6c79845994b
lbarto12 <74741908+lbarto12@users.noreply.github.com>
2024-12-03 01:41:58 -0500

    Day 3 Solution - Further Craziness

--- Day 3/main.py
+++ Day 3/main.py
@@ -5,11 +5,5 @@ with open('input.txt') as file:
 
     print('Part 1:', sum([a * b for a, b in [(int(c), int(d)) for c, d in re.findall(r'mul\((\d+),(\d+)\)', lines)]]))
 
-
     on = True
-
-    def flip(inst: str) -> bool:
-        global on
-        return (on := inst == 'do') and False if not (m := inst.startswith('mul')) else on and m
-
-    print('Part 2:', sum([int(a) * int(b) for inst, a, b in re.findall(r"(don't|do|mul\((\d+),(\d+)\))", lines) if flip(inst)]))
+    print('Part 2:', sum([int(a) * int(b) for inst, a, b in re.findall(r"(don't|do|mul\((\d+),(\d+)\))", lines) if ((on := inst == 'do') and False if not inst.startswith('mul') else on)]))
