e17d7bd8a9bf175a44462b0544d892782d69e7b3
lbarto12 <74741908+lbarto12@users.noreply.github.com>
2024-12-04 01:30:47 -0500

    Day 4 Solution If you're a hiring manager reviewing my code, do not base my worth off of the absolutely disgusting one-liners in here. I will never do this professionally :)

---
+++ Day 4/main.py
@@ -0,0 +1,20 @@
+from typing import List
+import re
+
+def count_xmas(board: List[str]) -> int:
+    return sum([line.count('XMAS') + line.count('SAMX') for line in board])
+
+def transpose(mtx: List[str]) -> List[str]:
+    return [''.join(s) for s in zip(*mtx)]
+
+
+with open('input.txt') as file:
+    lines: List[str] = [i.strip() for i in file.readlines()]
+
+    print(f"Part 1: {count_xmas(lines) + count_xmas(t := transpose(lines)) + count_xmas(transpose(['0' * (len(t) - i) + t[i] + '0' * i for i, _ in enumerate(t)])) + count_xmas(transpose(['0' * i + t[i] + '0' * (len(t) - i) for i, _ in enumerate(t)]))}")
+
+    # fuck checking out of bounds ;)
+    w: List[str] = (p := ['0' * (len(lines[0]) + 2)]) + ['0' + i + '0' for i in lines] + p
+    print(f"Part 2: {sum(w[i][j] == 'A' and {w[i - 1][j - 1], w[i + 1][j + 1]} == {w[i - 1][j + 1], w[i + 1][j - 1]} == {'M', 'S'} for j, _ in enumerate(w[0]) for i, _ in enumerate(w))}")
+
+
